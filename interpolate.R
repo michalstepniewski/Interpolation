b1767a6489599a6975b341b19090caaec4cc1557
michalstepniewski <michalstepniewski@gmail.com>
2016-06-03 20:21:42 +0200

    added some comments

--- interpolate.R
+++ interpolate.R
@@ -8,8 +8,12 @@ end_date <- "2016-01-07"
 query <- sprintf("
                  SELECT *
                  FROM PostsByReadDates
-                 ", start_date, end_date)
-#WHERE (created_date BETWEEN '%s' AND '%s')
+WHERE (created_date BETWEEN '%s' AND '%s')
+                                  ", start_date, end_date)
+# musze zrobic to zeby jakos dzialalo na cala baze
+# moge to robic po kawalku jakos
+# problem zeby tyle naraz wczytac do pamieci
+# mozna robic po kawalku datowym
 
 test <- dbGetQuery(db, query)
 
@@ -17,6 +21,24 @@ post_ids_query <- sprintf("SELECT DISTINCT post_id FROM PostsByReadDates")
 
 post_ids <- dbGetQuery(db, post_ids_query)
 InterpolatedMain <- test[0,]
+
+crt_tbl_command =sprintf("CREATE TABLE PostsByReadDatesInterpolated(Id INTEGER PRIMARY KEY,\
+                                                                      post_id TEXT,\
+ video_id TEXT,\
+ created_date TEXT,\
+ length TEXT,\
+ message TEXT,\
+ labels TEXT,\
+ origin TEXT,\
+ views INTEGER,\
+ likes INTEGER,\
+ comments INTEGER,\
+ shares INTEGER,\
+ read_date TEXT,\
+ minutes_elapsed INTEGER,\
+ hours_elapsed INTEGER,\
+ filename TEXT)");
+dbSendQuery(db, crt_tbl_command)
 
 for (post_id in unique(test$post_id)){
 
@@ -43,23 +65,7 @@ for (post_id in unique(test$post_id)){
     }
     Interpolated <- cbind(test2[1:dim(Zwynik)[1],1:8],  Zwynik[,2:5], unique(read_dates_full_hour), test2[1:dim(Zwynik)[1],14:16])
     InterpolatedMain <- rbind(InterpolatedMain,Interpolated)
+    dbWriteTable(db,"PostsByReadDatesInterpolated",InterpolatedMain)
 }
 #test2[1:8]
-crt_tbl_command =sprintf("CREATE TABLE PostsByReadDatesInterpolated(Id INTEGER PRIMARY KEY,\
-                                                                      post_id TEXT,\
- video_id TEXT,\
- created_date TEXT,\
- length TEXT,\
- message TEXT,\
- labels TEXT,\
- origin TEXT,\
- views INTEGER,\
- likes INTEGER,\
- comments INTEGER,\
- shares INTEGER,\
- read_date TEXT,\
- minutes_elapsed INTEGER,\
- hours_elapsed INTEGER,\
- filename TEXT)");
-test <- dbSendQuery(db, query)
-dbWriteTable(test,"PostsByReadDatesInterpolated",InterpolatedMain)
+
